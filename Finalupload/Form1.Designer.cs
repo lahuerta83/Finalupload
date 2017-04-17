521b44821b146440fbd2ed25942226c815f3f7aa
Leigh Ann <lahuerta83@gmail.com>
2017-04-17 16:03:38 -0400

    Add project files.

---
+++ Finalupload/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Finalupload
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Upload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(69, 161);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(281, 29);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 224);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Final Document upload";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Finalupload/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace Finalupload
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            label1.Text = "0";
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ThreadStart startprog = new ThreadStart(ReadFile);
+
+            Thread workprog = new Thread(startprog);
+
+            workprog.Start();
+
+        }
+
+        public delegate void updatebar();
+
+
+        private void UpdateProgress()
+        {
+            progressBar1.Value += 1;
+
+            // Here we are just updating a label to show the progressbar value
+            label1.Text = Convert.ToString(Convert.ToInt64(label1.Text) + 1);
+        }
+
+        private void ReadFile()
+        {
+            String finalfile = @"c:\\Users\Leigh Ann\Documents\finalfile.txt";
+
+            FileInfo fileSize = new FileInfo(finalfile);
+            long size = fileSize.Length;
+
+            long Sizecurrent = 0;
+            long incsize = (size / 100);
+
+            StreamReader stream = new StreamReader(new FileStream(finalfile, FileMode.Open));
+
+            char[] buff = new char[10];
+
+            // Read through the file until end of file
+            while (!stream.EndOfStream)
+            {
+                // Add to the current position in the file
+                Sizecurrent += stream.Read(buff, 0, buff.Length);
+
+
+                if (Sizecurrent >= incsize)
+                {
+                    Sizecurrent -= incsize;
+                    progressBar1.Invoke(new updatebar(this.UpdateProgress));
+                }
+            }
+
+            stream.Close();
+            
+            MessageBox.Show("Document has been upload!");
+
+
+
+        }
+    }
+}
